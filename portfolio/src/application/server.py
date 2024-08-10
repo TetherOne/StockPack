6d9cf0ad601a99b0f54bbf32822dd67608a5c13d
Pasha Ryaguzov <pashajobber@gmail.com>
2024-08-10 22:11:00 +0500

    added porfolio server

---
+++ portfolio/src/application/server.py
@@ -0,0 +1,16 @@
+from typing import TypeVar
+
+from fastapi import FastAPI
+
+
+FastAPIInstance = TypeVar("FastAPIInstance", bound=FastAPI)
+
+
+class ApiServer:
+    app_portfolio = FastAPI()
+    
+    def __init__(self, app: FastAPI):
+        self.__app = app
+
+    def get_app(self) -> FastAPIInstance:
+        return self.__app
